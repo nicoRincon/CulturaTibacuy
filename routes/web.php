e17bb7e61b4fec6f7944529dedde0a1e0f1ba01d
Nicolas Rincon <f.nicolasr23@gmail.com>
2025-04-24 20:02:08 -0500

    Agrega diagrama de clases mejorado para el sistema de gestión escolar

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,15 @@
-<?php
+<?php 
+//es el encargado de recibir todas las peticiones de nuestra pagina web
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+route::get('/posts', function () {
+    return "Aqui se veran todos los posts";
 });
 
 Auth::routes();
