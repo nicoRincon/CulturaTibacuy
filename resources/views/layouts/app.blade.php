2ae8a4ca20a62158663f4d0751e1911e99f57597
Nicolas Rincon <f.nicolasr23@gmail.com>
2025-05-13 14:05:54 -0500

    modificacion welcome

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,6 +21,7 @@
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" defer></script>
+    
 </head>
 <body>
     <div id="app">
