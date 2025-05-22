51de790a8201ee1ad079efb430ee6f9839d10194
nicoRincon <f.nicolasr23@gmail.com>
2025-05-22 14:58:33 -0500

    cambio en migraciones y auth

--- config/auth.php
+++ config/auth.php
@@ -65,10 +65,10 @@ return [
             'model' => App\Models\User::class,
         ],
 
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
+        'usuarios' => [
+            'driver' => 'database',
+            'table' => 'usuarios',
+        ],
     ],
 
     /*
--- database/migrations/2025_05_06_162008_create_usuarios_table.php
+++ database/migrations/2025_05_06_162008_create_usuarios_table.php
@@ -52,7 +52,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('usuarios');
         Schema::dropIfExists('password_reset_tokens');
     }
 };
